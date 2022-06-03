907c83880d93aa5eaf3de76e9965008a8d0e824d
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-03 09:16:14 +0300

    Add 05. Jourmey

---
+++ ConditionalStatementsAdvanced-Lab/05.Journey/05.Journey/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _05.Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            season = season.ToLower();
+            string destination = "";
+            string destinationIn = "";
+            double sum = 0; 
+            
+            if (budjet <= 100 )
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    sum =  (budjet * 0.3); destinationIn = "Camp";
+                }
+                else { sum = (budjet * 0.7); destinationIn = "Hotel"; }
+            }
+
+            if (budjet >100 && budjet <= 1000 )
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    sum = (budjet * 0.4); destinationIn = "Camp";
+                }
+                else { sum = (budjet * 0.8); destinationIn = "Hotel"; }
+            }
+            if (budjet > 1000)
+            {
+                destination = "Europe";
+                                
+                    sum = (budjet * 0.9); destinationIn = "Hotel";
+                
+            }
+            if (budjet > 0 )
+            {
+                Console.WriteLine($"Somewhere in {destination}");
+                Console.WriteLine($"{destinationIn} - {sum:f2}");
+            }
+        }
+        
+            
+    }
+}
