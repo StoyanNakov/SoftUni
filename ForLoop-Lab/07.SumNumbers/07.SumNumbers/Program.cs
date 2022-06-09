6e57fe495ec2d1097a4ee6002790dcfc68481dc0
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-09 08:58:06 +0300

    Add 07.SumNumbers

---
+++ ForLoop-Lab/07.SumNumbers/07.SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int number  = int.Parse(Console.ReadLine());
+                sum = sum + number;
+            }
+                Console.WriteLine(sum);
+        }
+    }
+}
