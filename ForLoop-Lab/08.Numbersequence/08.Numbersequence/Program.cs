ed1b97560ee44fe02e743a6240e9dfd3401fa853
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-09 08:57:39 +0300

    Add 08.Numbersequence

---
+++ ForLoop-Lab/08.Numbersequence/08.Numbersequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08.Numbersequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int minNum = int.MaxValue;
+            int maxNum = int.MinValue;
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                
+                if (maxNum < number) 
+                { 
+                    maxNum = number; 
+                }
+                if (minNum > number) 
+                {
+                    minNum = number; 
+                }
+
+            }
+            
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+        }
+    }
+}
