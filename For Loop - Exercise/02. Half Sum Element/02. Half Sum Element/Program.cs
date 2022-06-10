1184b04cedd5a4a841f0e49f386fc93c9310dc14
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-10 10:01:18 +0300

    Add 01 and 02

---
+++ For Loop - Exercise/02. Half Sum Element/02. Half Sum Element/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MaxValue;
+            for (int i = 0; i < numbers; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            int sumWithoutMaxNumber = sum - max;
+                if (max == sumWithoutMaxNumber)
+                {
+                    Console.WriteLine("Yes");
+                    Console.WriteLine($"Sum = {max}");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    Console.WriteLine($"Diff = {Math.Abs(max - sumWithoutMaxNumber)}");
+                }
+
+
+
+
+           
+        }
+    }
+}
