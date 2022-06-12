b2b0317d10062ff0ce41ff719976c492f4e8ef0a
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-12 09:56:06 +0300

    Add new

---
+++ While Loop - Lab/07. Min Number/07. Min Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+
+            int min = int.MaxValue;
+
+            while (num != "Stop")
+            {
+                int number = int.Parse(num);
+
+                if (number < min)
+                {
+                    min = number;
+                }
+
+                num = Console.ReadLine();
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
