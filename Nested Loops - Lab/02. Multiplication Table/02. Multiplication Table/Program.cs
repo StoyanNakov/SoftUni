9153fd867112b2d2ccf6e9c9751dbd77296e860b
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-13 08:28:55 +0300

    Add 02. Multiplication Table

---
+++ Nested Loops - Lab/02. Multiplication Table/02. Multiplication Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+            for (first = 1; first <=10; first++)
+            {
+                for (second = 1; second <= 10; second++)
+                
+                    Console.WriteLine($"{first} * {second} = {first*second}");
+            }
+
+           
+        }
+    }
+}
