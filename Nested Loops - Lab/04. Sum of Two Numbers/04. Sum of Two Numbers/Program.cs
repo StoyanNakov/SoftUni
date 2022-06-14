d78b948ae2b846b2bfc714e989ee3ca77e5a5704
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-14 23:55:43 +0300

    Add

---
+++ Nested Loops - Lab/04. Sum of Two Numbers/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int combination = 0;
+           int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++) 
+            {
+                if (start + i == magicNumber)
+                {
+                    combination++;
+                    Console.WriteLine(i);
+                }
+            }
+
+            
+        }
+    }
+}
