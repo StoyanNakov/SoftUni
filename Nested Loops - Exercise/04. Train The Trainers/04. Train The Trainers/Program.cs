0edfe50bb687882e48bbebae5081bf767db7507c
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-18 08:44:14 +0300

    Add

---
+++ Nested Loops - Exercise/04. Train The Trainers/04. Train The Trainers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopple = int.Parse(Console.ReadLine());
+
+            int countGrades = 0;
+            double allGrades = 0;
+            string presName;
+
+            while ((presName = Console.ReadLine()) != "Finish")
+            {
+                double totalGrades = 0;
+
+                for (int i = 1; i <= peopple; i++)
+                {
+                    double grades = double.Parse(Console.ReadLine());
+                    totalGrades += grades;
+                    allGrades += grades;
+                    countGrades++;
+                }
+
+                Console.WriteLine($"{presName} - {totalGrades / peopple:f2}.");
+            }
+
+            Console.WriteLine($"Student's final assessment is {allGrades / countGrades:f2}.");
+        }
+    }
+}
