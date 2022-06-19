98270ea8b74ddb1d12f4e78e4ece9caec1d6723d
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-19 18:21:28 +0300

    Update

---
+++ PB C# may 2022/Programming Basics Online Pre - Exam - 18 and 19 June 2022/01. Christmas Preparation/01. Christmas Preparation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Christmas_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double paper = double.Parse(Console.ReadLine());
+            double plat = double.Parse(Console.ReadLine());
+            double glue = double.Parse(Console.ReadLine());  
+            double discount = double.Parse(Console.ReadLine());
+
+            double sumPaper = paper * 5.80;
+            double sumPlat = plat * 7.2;
+            double sumGlue = glue * 1.2;
+            double total = sumPaper + sumPlat + sumGlue;
+            double discountValue = (total * discount) / 100; 
+            Console.WriteLine($"{total - discountValue:f3}");
+            
+        }
+    }
+}
