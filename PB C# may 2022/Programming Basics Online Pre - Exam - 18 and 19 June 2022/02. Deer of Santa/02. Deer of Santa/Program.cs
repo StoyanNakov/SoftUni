26eb961fd479aa9aabad96086a528bd557742bd3
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-20 08:23:40 +0300

    Add Deer Santa

---
+++ PB C# may 2022/Programming Basics Online Pre - Exam - 18 and 19 June 2022/02. Deer of Santa/02. Deer of Santa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Deer_of_Santa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double daysOut = double.Parse(Console.ReadLine());
+            double food = double.Parse(Console.ReadLine());
+            double foodForFirst = double.Parse(Console.ReadLine());
+            double foodForSecond = double.Parse(Console.ReadLine());
+            double foodForThird = double.Parse(Console.ReadLine());
+
+            double needForAll = (daysOut * foodForFirst) + (daysOut * foodForSecond) + (daysOut * foodForThird);
+            if (food >= needForAll)
+            {
+                Console.WriteLine($"{Math.Floor(food - needForAll)} kilos of food left.");
+            }
+            else 
+            {
+                Console.WriteLine($"{Math.Ceiling(needForAll - food)} more kilos of food are needed.");
+            }
+        }
+    }
+}
