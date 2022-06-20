d6985ab2f928c0b53dbc73de5457bf00426f6e69
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-20 08:22:47 +0300

    Add Hair Salon

---
+++ PB C# may 2022/Programming Basics Online Pre - Exam - 18 and 19 June 2022/05. Hair Salon/05. Hair Salon/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Hair_Salon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetMoney = int.Parse(Console.ReadLine());
+            int sum = 0;
+            string service = "";
+            string details = "";
+            while ((service = Console.ReadLine()) != "closed")
+            {
+                details = Console.ReadLine();
+                if (service == "haircut")
+                {
+                    if (details == "mens") { sum = sum + 15; }
+                    if (details == "ladies") { sum = sum + 20; }
+                    if (details == "kids") { sum = sum + 10; }
+                }
+                if (service == "color")
+                {
+                    if (details == "touch up") { sum = sum + 20; }
+                    if (details == "full color") { sum = sum + 30; }
+                    
+                }
+            
+                if (sum >= targetMoney) { break; }
+            }
+            if (sum >= targetMoney)
+            {
+                Console.WriteLine($"You have reached your target for the day!");
+            }
+            else 
+            {
+                Console.WriteLine($"Target not reached! You need {targetMoney - sum}lv. more.");
+            }
+                Console.WriteLine($"Earned money: {sum}lv.");
+
+        }
+    }
+}
