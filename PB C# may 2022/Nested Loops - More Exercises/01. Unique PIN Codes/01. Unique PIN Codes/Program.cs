96db9d749dc943f1bc35118f83b5427607ca3a52
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-22 08:43:43 +0300

    Update

---
+++ PB C# may 2022/Nested Loops - More Exercises/01. Unique PIN Codes/01. Unique PIN Codes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Unique_PIN_Codes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    for (int k = 1; k <= c; k++)
+                    {
+                        if (i % 2 == 0 && k % 2 == 0)
+                        {
+                            if (j == 2 || j ==3 || j == 5 || j == 7 )
+                            {
+                                Console.WriteLine($"{i} {j} {k}");
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
