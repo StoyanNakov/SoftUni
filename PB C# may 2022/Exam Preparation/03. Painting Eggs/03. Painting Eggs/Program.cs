525ab7442e59b0b1cd9e3ec1e6bd668adc6580cd
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-22 08:44:23 +0300

    Update

---
+++ PB C# may 2022/Exam Preparation/03. Painting Eggs/03. Painting Eggs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _03._Painting_Eggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string size = Console.ReadLine();
+            string color = Console.ReadLine();
+            int batches = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+           
+            if (color == "Red")
+            {
+                
+                if (size == "Large")
+                {
+                    price = 16.00;
+                }
+                else if (size == "Medium")
+                {
+                    price = 13.00;
+                }
+                else 
+                {
+                    price = 9.00;
+                }
+            }
+            else if (color == "Green")
+            {
+                
+                if (size == "Large")
+                {
+                    price = 12.00;
+                }
+                else if (size == "Medium")
+                {
+                    price = 9.00;
+                }
+                else 
+                {
+                    price = 8.00;
+                }
+            }
+            else 
+            {
+                
+                if (size == "Large")
+                {
+                    price = 9.00;
+                }
+                else if (size == "Medium")
+                {
+                    price = 7.00;
+                }
+                else 
+                {
+                    price = 5.00;
+                }
+            }
+
+          
+            double profit = batches * price;
+            
+            double expenses = profit * 35 / 100;
+            
+            profit -= expenses;
+
+           
+            Console.WriteLine($"{profit:F2} leva.");
+        }
+    }
+}
