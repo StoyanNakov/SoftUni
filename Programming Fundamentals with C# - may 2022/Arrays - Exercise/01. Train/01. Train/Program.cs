3fc177a7993e74171802de18d560ede041117030
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-22 08:43:27 +0300

    CSharp-Fundamentals

---
+++ Programming Fundamentals with C# - may 2022/Arrays - Exercise/01. Train/01. Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] people = new int[wagons];
+            int sum = 0;
+            for (int j = 0; j < wagons; j++)
+            {
+                int consoleInWagone = int.Parse(Console.ReadLine());
+                people[j] = consoleInWagone;
+            }
+
+            foreach (int i in people)
+            {
+                sum += i;
+                Console.Write($"{i} ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
