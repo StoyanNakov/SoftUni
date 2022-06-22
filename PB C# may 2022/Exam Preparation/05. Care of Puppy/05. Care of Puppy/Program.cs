5fea0334c1f081c64414a13447904733183b844c
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-22 08:44:05 +0300

    Update

---
+++ PB C# may 2022/Exam Preparation/05. Care of Puppy/05. Care of Puppy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Care_of_Puppy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int boughtFood = int.Parse(Console.ReadLine()) * 1000;
+            int eatenFood = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "Adopted")
+            {
+                eatenFood += int.Parse(input);
+              
+                input = Console.ReadLine();
+            }
+
+            if (boughtFood >= eatenFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {boughtFood - eatenFood} grams.");
+            }
+           
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {eatenFood - boughtFood} grams more.");
+            }
+        }
+    }
+}
