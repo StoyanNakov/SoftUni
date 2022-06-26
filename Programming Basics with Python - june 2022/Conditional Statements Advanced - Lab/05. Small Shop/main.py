b7c754962ba12cee164134a816922a673def4d18
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-06-27 00:48:05 +0300

    Add 05. Small Shop

---
+++ Programming Basics with Python - june 2022/Conditional Statements Advanced - Lab/05. Small Shop/main.py
@@ -0,0 +1,41 @@
+product = input()
+town = input()
+quantity = float(input())
+price = 0;
+if town == 'Sofia':
+    if product == 'coffee':
+        price = 0.5
+    if product == 'water':
+        price = 0.8
+    if product == 'beer':
+        price = 1.2
+    if product == 'sweets':
+        price = 1.45
+    if product == 'peanuts':
+        price = 1.6
+
+if town == 'Plovdiv':
+    if product == 'coffee':
+        price = 0.4
+    if product == 'water':
+        price = 0.7
+    if product == 'beer':
+        price = 1.15
+    if product == 'sweets':
+        price = 1.30
+    if product == 'peanuts':
+        price = 1.50
+
+if town == 'Varna':
+    if product == 'coffee':
+        price = 0.45
+    if product == 'water':
+        price = 0.7
+    if product == 'beer':
+        price = 1.1
+    if product == 'sweets':
+        price = 1.35
+    if product == 'peanuts':
+        price = 1.55
+
+print (quantity * price)
