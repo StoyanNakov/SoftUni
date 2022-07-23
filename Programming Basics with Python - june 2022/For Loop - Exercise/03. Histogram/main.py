d182b63a88e7202c18eb489307b6e518d5071bfa
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-07-23 20:11:20 +0300

    Add new

---
+++ Programming Basics with Python - june 2022/For Loop - Exercise/03. Histogram/main.py
@@ -0,0 +1,24 @@
+n = int(input())
+p1 = 0
+p2 = 0
+p3 = 0
+p4 = 0
+p5 = 0
+
+for _ in range(n):
+    current_number = int(input())
+    if current_number < 200:
+        p1+=1
+    elif current_number < 400:
+        p2+=1
+    elif current_number < 600:
+        p3+=1
+    elif current_number < 800:
+        p4+=1
+    elif current_number >= 800:
+        p5+=1
+print(f'{p1 / n * 100:.2f}%')
+print(f'{p2 / n * 100:.2f}%')
+print(f'{p3 / n * 100:.2f}%')
+print(f'{p4 / n * 100:.2f}%')
+print(f'{p5 / n * 100:.2f}%')
