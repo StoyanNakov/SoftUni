2c44af84b33c556d5019d85000e5fe600e552602
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-08-05 22:16:02 +0300

    Create main.py

---
+++ Programming Basics with Python - june 2022/For Loop - Lab/06. Vowels Sum/main.py
@@ -0,0 +1,14 @@
+input = input()
+sum = 0;
+for char in input:
+    if char =='a':
+        sum =sum + 1
+    if char =='e':
+        sum = sum + 2
+    if char == 'i':
+        sum = sum + 3
+    if char == 'o':
+        sum = sum + 4
+    if char == 'u':
+        sum = sum + 5
+print(sum)
